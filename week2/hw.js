1efc1668756a387010fbf65c31515e2222d0a320
Gaorieh Isaac <gaorieh@hotmail.com>
2018-02-12 11:15:53 +0100

    add new files

---
+++ week2/hw.js
@@ -0,0 +1,4 @@
+//1. Write a console.log statement saying "Hello World!" for each language that you know.
+console.log("Привет мир \!"); //Russian
+console.log("Ciao mondo \!"); //Italian
+console.log("Hola món \!");  //Catalan
