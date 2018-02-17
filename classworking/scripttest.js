cd62eaa22975e3a146399832ef8d3c6ca7805ee2
Gaorieh Isaac <gaorieh@hotmail.com>
2018-02-17 18:53:15 +0100

    add files to workingclass folder

---
+++ classworking/scripttest.js
@@ -0,0 +1,5 @@
+//alert("Hello World");
+let x = number(prompt("Enter a number1: ", ""));
+let y = number(prompt("Enter a number2: ", ""));
+let z = x + y;
+document.write("The sum of " + x + y + " = " + z);
