279da8c1d1ca7ff0cd71b8b82f564d291624b9a5
Gaorieh Isaac <gaorieh@hotmail.com>
2018-02-17 14:49:12 +0100

    sx

--- classworking/test.js
+++ classworking/test.js
@@ -5,15 +5,9 @@ for (var i = 1; i <= 7; i++) {
     }
     console.log(str);
 }
-<<<<<<< HEAD
 //output
 /*
 
-=======
-
-//output
-/*
->>>>>>> e681b33511dc168e345bdcac5f6f328c83e0eb44
 *
 **
 ***
@@ -24,6 +18,7 @@ for (var i = 1; i <= 7; i++) {
 <<<<<<< HEAD
 
 */
-=======
-*/
->>>>>>> e681b33511dc168e345bdcac5f6f328c83e0eb44
+===
+=== = *
+/ >>>
+>>> > e681b33511dc168e345bdcac5f6f328c83e0eb44
