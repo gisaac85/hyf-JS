e5fc221fa347f4a07e2149338b24feef7f8c6e65
Gaorieh Isaac <gaorieh@hotmail.com>
2018-02-17 14:47:25 +0100

    aa

--- classworking/test.js
+++ classworking/test.js
@@ -1,7 +1,19 @@
-for (var i = 7; i >= 1; i--) {
+for (var i = 1; i <= 7; i++) {
     var str = "";
-    for (var j = i; j <= 7; j++) {
+    for (var j = i; j >= 1; j--) {
         str += "*";
     }
     console.log(str);
 }
+//output
+/*
+
+*
+**
+***
+****
+*****
+******
+*******
+
+*/
