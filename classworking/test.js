7523716c96e2a745dbde5724322964d6434b0dc5
Gaorieh Isaac <gaorieh@hotmail.com>
2018-02-21 21:18:16 +0100

    ff

--- classworking/test.js
+++ classworking/test.js
@@ -28,3 +28,21 @@ let part2 = vehicles.slice(length - 1); // gets bus
 advertisement += part1.join(", ") + " and " + part2.join('') + "."; //till bus puts "," in between  for the last index bus puts "."
 
 console.log(advertisement);
+
+let x = [1, 2, 3];
+let y = [1, 2, 3];
+let z = y;
+
+console.log('I think x == y will be True');
+console.log(x == y);
+
+console.log('I think x === y will be True');
+console.log(x === y);
+
+console.log('I think z == y will be True');
+console.log(z == y);
+
+console.log('I think z == x will be False');
+console.log(z == x);
+
+console.log('\n');
